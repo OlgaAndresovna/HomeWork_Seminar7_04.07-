69a8ce793e201650fbd5501dd3f39049bb8366c0
Olga <olg200919@ya.ru>
2022-07-09 16:26:33 +0300

    Семинар_8 от 07. 07, задача 56

---
+++ HomeWorkTask_56/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+int m = 4;
+int n = 8;
+int[,] matrix = new int[m, n];
+for (int i = 0; i < m; i++) 
+{
+    for (int j = 0; j < n; j++) 
+    {
+        matrix[i, j] = new Random().Next(1, 10); 
+        Console.Write(matrix[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+
+int minsum = Int32.MaxValue; //представления максимального значения 
+int indexLine = 0;
+int sum = 0;
+
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        sum = sum + matrix[i, j];        
+    }
+    if (sum < minsum)
+    {
+        minsum = sum;
+        indexLine++;
+    }
+}
+
+Console.WriteLine($"Строка с наименьшей суммой элементов под номером: " + (indexLine) + ", с суммой элементов = " + (minsum));
